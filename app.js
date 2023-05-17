5011a251ea7d1ffa60db5a983451968053bb2126
DanielHernandezO <daniel.hernandez.54361@gmail.com>
2023-05-16 22:13:11 -0500

    added Dockerfile

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path')
 const {routes} = require('./webserver/routes');
 const {startServer} = require('./webserver/server')
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.set('views',path.join(__dirname,'application/views'));
 app.set('view engine','ejs')
